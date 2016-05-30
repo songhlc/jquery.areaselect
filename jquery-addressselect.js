26ffd3772b08dd56cbc392a40b0f08c5efa5aa59
windknow <songhlc@yonyou.com>
2016-05-30 11:12:36 +0800

    修改项目地址链接

--- jquery-addressselect.js
+++ jquery-addressselect.js
@@ -7,7 +7,7 @@
  *   http://www.opensource.org/licenses/mit-license.php
  *
  * Project home:
- *   https://github.com/songhlc/
+ *   https://github.com/songhlc/jquery.areaselect
  *
  * Version:  1.0.0
  *
