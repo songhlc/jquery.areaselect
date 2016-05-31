a9c0222aa05308496d245af26b01d850a95e82c0
windknow <songhlc@yonyou.com>
2016-05-31 10:47:51 +0800

    添加传入数据格式自定义,修改部分bug

--- jquery-addressselect.js
+++ jquery-addressselect.js
@@ -13,7 +13,7 @@
  *
  */
 (function($){
-    $.fn.arealikeselect = function(options){
+    $.fn.areaselect = function(options){
         var element = this;
         var settings = {
             inputCls:"form-control",
@@ -36,12 +36,12 @@
             $.extend(settings, options);
         }
         function pushSelectedData(curindex,data){
-            selectedDatas.ids.push(data.id);
-            selectedDatas.names.push(data.name);
-            for(var i=selectedDatas.ids.length-1;i>curindex;i--){
+            for(var i=selectedDatas.ids.length-1;i>=curindex;i--){
                 selectedDatas.ids.pop();
                 selectedDatas.names.pop();
             }
+            selectedDatas.ids.push(data.id);
+            selectedDatas.names.push(data.name);
             selectedDatas.fullname="";
             for(var i=0;i<selectedDatas.names.length;i++){
                 selectedDatas.fullname+=selectedDatas.names[i];
@@ -67,12 +67,15 @@
 
             element.undelegate().delegate(".as-content a","click",function(){
                 var selectedData = {id:$(this).attr("data-id"),name:$(this).html()};
+                //先看当前是第几条数据
+                var index=$(this).parents("ul").index();
                 //ajax查数据
                 settings.loadData(selectedData,function(datalist){
-                    if(datalist&&datalist.length>0){
+                    if(datalist&&((datalist.data&&datalist.data.length>0)||datalist.length>0)){
+                        if(datalist.keyid){
+                            datalist = formatData(datalist.keyid,datalist.keyname,datalist.data);
+                        }
 
-                        //先看当前是第几条数据
-                        var index=$(this).parents("ul").index();
                         //test
                         //if(index>2){
                         //    settings.onok(selectedDatas);
@@ -102,10 +105,12 @@
                         pushSelectedData(index,selectedData);
                     }
                     else{
+                        pushSelectedData(index,selectedData);
                         settings.onok(selectedDatas);
                         element.find("input").val(selectedDatas.fullname);
+                        $(element).removeClass("as-hover");
                     }
-                }.bind(this));
+                }.bind(this),index);
 
             });
             element.delegate(".as-tabs a","click",function() {
@@ -139,9 +144,22 @@
                 },300);
             });
         }
+        function formatData(keyid,keyname,data){
+            var datalist = [];
+            for(var i=0;i<data.length;i++){
+                var dataitem ={};
+                dataitem.id = data[i][keyid];
+                dataitem.name=data[i][keyname];
+                datalist.push(dataitem);
+            }
+            return datalist;
+        }
         function init(){
             if(settings.initData){
-                var linkelement = settings.type=="link"?'<a class="as-linkle">'+settings.linktext+'</a>':'<input placeholder="'+settings.linktext+'" class="as-linkle" type="text">';
+                if(settings.initData.keyid){
+                    settings.initData =formatData(settings.initData.keyid,settings.initData.keyname,settings.initData.data);
+                }
+                var linkelement = settings.type=="link"?'<a class="as-linkle">'+settings.linktext+'</a>':'<input placeholder="'+settings.linktext+'" class="as-linkle '+settings.inputCls+'" type="text">';
                 var tpl = linkelement+
                     '<div class="as-ctn">'+
                     '<div class="as-tabs">'+
@@ -152,7 +170,7 @@
                     parseItemTemplate(settings.initData)+
                     '</div>'+
                     '</div>';
-                element.append(tpl);
+                element.html('').append(tpl);
                 if(settings.defaultFullname){
                     element.find("input").val(settings.defaultFullname);
                 }
--- readme.md
+++ readme.md
@@ -24,3 +24,39 @@
 		{id:2,name:"北京"},{id:5,name:"上海"},
 		{id:4,name:"天津"},{id:6,name:"成都"}
     ];
+
+###自定义数据格式
+
+    默认数据格式是{id:"id",name:"name"}
+    如果需要自定义数据格式,改成如下即可{keyid:"ProID",keyname:"ProName",data:{ProID:"ProID",ProName:"ProName"}},
+    initData和loadData里返回的都按这种格式传入即可
+
+    一个小例子:
+
+    $("#areabelong").areaselect({
+        initData:{keyid:"ProID",keyname:"ProName",data:province},
+        loadData:function(selectedData,callback,index){
+            if(index ==0){
+                viewModel.province_code(selectedData.id);
+                callback({keyid:"CityID",keyname:"CityName",data:viewModel.cities()});
+            }
+            else if(index==1){
+                viewModel.city_code(selectedData.id);
+                callback({keyid:"DisCode",keyname:"DisName",data:viewModel.districts()});
+            }
+            else{
+                viewModel.county_code(selectedData.id);
+                callback(null);
+            }
+
+        },
+        onok:function(selectedDatas){
+            viewModel.province_code(selectedDatas.ids[0]);
+            viewModel.areafullname(selectedDatas.fullname);
+        },
+        type:"input",
+        linktext:"请选择收货地区",
+        defaultFullname:viewModel.areafullname()
+    })
+
+
